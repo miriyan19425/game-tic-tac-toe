87dc53b12360993f32f8fd0446c6fa3313537b96
miriyan19425 <miriyan19425@gmail.com>
2025-04-23 01:19:17 +0300

    Update GameBoard.jsx

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
 import Button from './Button';
 
 const emptyGameBoard = new Array(9).fill(null);
 
+function fillGameBoard(fSqs) {
+  const gameBoard = emptyGameBoard;
+  for (const fSq of fSqs) { gameBoard[fSq.sqInd] = fSq.sqMark }
+  return gameBoard;
+}
+
 function GameBoard() {
+  const [filledSquares, setFilledSquares] = useState([]);
+
+  const currentGameBoard = fillGameBoard(filledSquares);
+
+  function addNewMark(ind){
+    setFilledSquares( prev => [{ sqInd: ind, sqMark: prev[0]?.sqMark === 'X' ? 'O' : 'X'}, ...prev] );
+  }
 
   return  (
     <ul>
-     { emptyGameBoard.map( (_, i) => <li key={i}><Button /></li> ) }
+     { currentGameBoard.map( (mark, i) => <li key={i}><Button handleOnClick={() => addNewMark(i)}>{mark}</Button></li> ) }
     </ul>   
   );
   
