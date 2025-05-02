d3e7ae9c4d608aff79bfe170c550b2e59108fd1c
miriyan19425 <miriyan19425@gmail.com>
2025-05-02 18:08:32 -0500

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 function Button({handleOnClick, children, winIndxComb, indx}){
 	return <button type='button' 
 			onClick={handleOnClick} 
-			className={winIndxComb && winIndxComb.includes(indx) ? 'mark win' : 'mark'}
+			className={children === 'New Game' ? 'new' : winIndxComb && winIndxComb.includes(indx) ? 'mark win' : 'mark'}
 			disabled={winIndxComb !== undefined}>
 		     {children}
 		</button>;
