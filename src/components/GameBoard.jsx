919171bafd8927afde14fe3883e75df16f1a99d7
miriyan19425 <miriyan19425@gmail.com>
2025-05-10 21:49:23 +0300

    Update GameBoard.jsx

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react';
 import Button from './Button';
 
 const emptyGameBoard = new Array(9).fill(null);
 const winCombs = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ];
 
 function fillGameBoard(fSqs) {
-  const gameBoard = emptyGameBoard;
+  const gameBoard = [...emptyGameBoard];
   for (const fSq of fSqs) { gameBoard[fSq.sqInd] = fSq.sqMark; }
   return gameBoard;
 }
@@ -18,16 +17,9 @@ function checkWinner(board) {
   return winner;
 }
 
-function GameBoard() {
-  const [filledSquares, setFilledSquares] = useState([]);
-
+function GameBoard({filledSquares, addNewMark}) {
   const currentGameBoard = fillGameBoard(filledSquares);
   const winIndexes = checkWinner(currentGameBoard);
-
-  function addNewMark(ind){
-    setFilledSquares( prev => [{ sqInd: ind, sqMark: prev[0]?.sqMark === 'X' ? 'O' : 'X'}, ...prev] );
-  }
-
   return  (
     <ul>
      { currentGameBoard.map( (mark, i) => <li key={i}><Button handleOnClick={() => addNewMark(i)} winIndxComb={winIndexes} indx={i}>{mark}</Button></li> ) }
