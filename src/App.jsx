4c594f53f49b6886cee3dfdfe4d164811fb7033e
miriyan19425 <miriyan19425@gmail.com>
2025-05-10 21:46:04 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import GameBoard from './components/GameBoard';
 import Button from './components/Button';
 
 function App() {
+  const [filledSquares, setFilledSquares] = useState([]);
+
+  function addNewMark(ind){
+    setFilledSquares( prev => [{ sqInd: ind, sqMark: prev[0]?.sqMark === 'X' ? 'O' : 'X'}, ...prev] );
+  }
+
+  function startNewGame(){
+    setFilledSquares([]);
+  }
+
   return (
     <>
-      <GameBoard />
-      <Button>New Game</Button>
+      <GameBoard filledSquares={filledSquares} addNewMark={addNewMark}/>
+      <Button handleOnClick={startNewGame}>New Game</Button>
     </>
   );
 } 
